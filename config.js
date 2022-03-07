4ed0ec711d6a6ffcbd7ef9c1b3bebfbd582218db
Thanh633 <100706860+Thanh633@users.noreply.github.com>
2022-03-08 01:16:14 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 // 1. Chỉnh sửa các dòng chữ theo mục đích của bạn
 const CONFIG = {
     introTitle: '🖐🖐🖐 Hee loo ! 🖐🖐🖐',
-    introDesc: `Anh có điều này muốn nói`,
+    introDesc: `Tôi Có Một Bài Test Nhỏ Cho Bạn`,
     btnIntro: 'OK',
-    title: 'Chúc em 8/3 vui vẻ nha 😙',
-    desc: 'Em có Thích Sự Bất Ngờ Này Không :> ',
-    btnYes: 'Có, em thật sự rất thích 💖💖💖',
-    btnNo: '💔 Anh mơ à ! 💔 ',
-    question:'Nói Cho Anh Biết Vì Sao Em Thích Quà 😊😊',
+    title: 'Bài Này Rất Đơn Giản là 1+1=?🙄',
+    desc: 'lưu ý không được gian lận nhé :> ',
+    btnYes: '1+1=3',
+    btnNo: '1+1=2 ',
+    question:'Tôi Thật Sự Rất Thất Vọng Về Bạn',
     reasonPlaceholder: 'Lý do',
     btnReply: 'Gởi ! 💌💌',
-    reply: 'Tại vì em yêu nó như yêu anh 😊😊',
-    mess: 'Anh biết mà, Anh cũng thích em lắm 🥰. Love you 300.000 😘😘',
-    messDesc: '💬💬 Nhắn tin cho Anh nha. 💬💬',
-    btnAccept: 'Ok lun 💖',
+    reply: 'Em Xin Lỗi Thầy Thành Mà🥺',
+    mess: 'Được Rồi Ib Thầy để được dạy kèm nhá',
+    messDesc: '💬💬 Nhắn tin cho Thầy nha. 💬💬',
+    btnAccept: ' Vâng ạ',
     messLink : 'https://www.facebook.com/thanhchill1967lofi'
 }
 At  in
